b3ee09300f7e0973ab15cf3d43d4a59fa10c0c9f
Alfayed <baksha97@gmail.com>
2022-02-10 23:30:18 -0500

    init

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv, dotenv_values
+import discord
+
+# load_dotenv() 
+config = dotenv_values(".env")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(config["BOT_TOKEN"])
