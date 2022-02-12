b58b122d459d31c17969c823fcf4f513cf21973d
Alfayed <baksha97@gmail.com>
2022-02-11 21:32:21 -0500

    duplicate wordles and

--- src/wordle.py
+++ src/wordle.py
@@ -1,6 +1,8 @@
 import re
 import pandas as pd
-import datetime
+from datetime import datetime, date
+
+
 # Helpers
 
 
@@ -17,19 +19,32 @@ def find_try_ratio(wordle_share_msg_header: str):
     max_attempts = int(header[-1])
     return won_on_try, max_attempts
 
+
 # State
 
 
 class WordleHistoryState:
+
     def __init__(self):
         self.wordle_df = pd.DataFrame(columns=[
             'player_id',  # str
             'wordle_id',  # int
             'won_on_try_num',  # optional<int>
             'total_num_tries',  # int
-            'created_date'])  # int
+            'created_date'  # int
+        ])
 
-    def add_wordle(self, player_id: str, wordle_id: int, won_on_try_num: int, total_num_tries: int, created_date: datetime.datetime):
+    # TODO: Remove this and just prevent duplicates in add
+    def __prepare_for_computation__(self):
+        """
+        We can lazily prepare for a computation if needed here.
+            - Prevents duplicate counts of shares.
+                - it would be better if we can just overwrite duplicates on add_wordle()
+        """
+        self.wordle_df = self.wordle_df.drop_duplicates(subset=['player_id', 'wordle_id'], keep='last')
+
+    def add_wordle(self, player_id: str, wordle_id: int, won_on_try_num: int, total_num_tries: int,
+                   created_date: datetime):
         self.wordle_df = self.wordle_df.append({'player_id': player_id,
                                                 'wordle_id': wordle_id,
                                                 'won_on_try_num': won_on_try_num,
@@ -37,14 +52,51 @@ class WordleHistoryState:
                                                 'created_date': created_date},
                                                ignore_index=True)
 
-    def create_all_stats_df(self):  #
-        allstatsdf = pd.DataFrame()
+    def compute_all_stats_df(self):  #
+        """
+        :returns: pandas.DataFrame with columns: *sorted
+            index                          int64
+            player_id                        str
+            total_games                    int64
+            avg_won_on_attempt           float64
+            win_percent                  float64
+            started_date          datetime64[ns]
+        """
+        self.__prepare_for_computation__()
+        all_stats_df = pd.DataFrame()
         groupedby_players = self.wordle_df.groupby(self.wordle_df.player_id)
         wordle_df = self.wordle_df
-        allstatsdf["total_games"] = groupedby_players.size()
-        allstatsdf["avg_won_on_attempt"] = groupedby_players.won_on_try_num.mean()
-        allstatsdf['win_percent'] = (wordle_df[wordle_df['won_on_try_num'].notna()].groupby(
+        all_stats_df["total_games"] = groupedby_players.size()
+        all_stats_df["avg_won_on_attempt"] = groupedby_players.won_on_try_num.mean()
+        all_stats_df['win_percent'] = (wordle_df[wordle_df['won_on_try_num'].notna()].groupby(
             wordle_df.player_id).size() / wordle_df.groupby(wordle_df.player_id).size()) * 100
-        allstatsdf['started_date'] = groupedby_players.created_date.min()
-        return allstatsdf.sort_values(["avg_won_on_attempt", "win_percent", "started_date"],
-                                      ascending=(True, False, True)).round(1).reset_index()
+        all_stats_df['started_date'] = groupedby_players.created_date.min()
+        return all_stats_df.sort_values(["avg_won_on_attempt", "win_percent", "started_date"],
+                                        ascending=(True, False, True)).round(1).reset_index()
+
+    def compute_daily_df(self, top=None):
+        """
+        :returns:
+            avg attempts for wins
+            avg of people who won
+            pandas.DataFrame with columns: *sorted
+                player_id                  object
+                wordle_id                  object
+                won_on_try_num            float64
+                total_num_tries            object
+                created_date       datetime64[ns]
+        """
+        self.__prepare_for_computation__()
+        df = self.wordle_df
+        df = df.loc[(date.today() == df['created_date'].dt.date)]
+        wid = df.wordle_id.value_counts().max()
+        # df = df.loc[(wid == df['wordle_id'])]  # only show the most used wordle id to filter out bad
+        percent_of_winners = df.won_on_try_num.notna().mean()
+        df.won_on_try_num = pd.to_numeric(df.won_on_try_num)
+        avg_turn_won = df.won_on_try_num.mean()
+        if top:
+            df = df.nlargest(top, 'won_on_try_num')
+
+        df = df.sort_values(["won_on_try_num", "created_date"], ascending=(True, True))
+        df = df.reset_index(drop=True)
+        return wid, avg_turn_won, percent_of_winners, df
