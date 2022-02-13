5625d7d9ea4ea8475741363abb8239a0219d0149
Alfayed Baksh <baksha97@gmail.com>
2022-02-12 22:04:56 -0500

    Develop (#15)
    * Add files via upload
    
    * adds tests
    
    * update ignore
    
    * update ignore
    
    * working embed, but seems to be missing games
    
    * cleanup
    
    * fix test bug and hardcoded wordle channel for debugging
    
    * created foundations, just need to fix ad hoc storing of messages
    
    * starting to fix on-go import
    
    * fixed missing new wordles
    
    * cleanup prints
    
    * Create README.md
    
    * removes pycache
    
    * cleanup
    
    * cleanup
    
    * removes checkpoints
    
    * rename job
    
    * updates metrics and cleanup
    
    * undo test config
    
    * fix typo
    
    * cleanup + comments
    
    * cleanup, plus bug fix when processing all messages
    
    * pre dict funcs
    
    * cleanup
    
    * duplicate wordles and
    
    * self host deploy workflow test
    
    * edits self deploy to kill others and detach
    
    * hyphen
    
    * adds continue-on-error for docker kill
    
    * change to wd only
    
    * edit ui
    
    * fixes today wid
    
    * update dt, at time check command
    
    * timezone debugging on host machine
    
    * cleanup
    
    * Update selfhosted_deploy_workflow.yml
    
    * adds timezone apply for incoming messages
    
    * moves today time fix into section of func instead (#12)
    
    * moves today time fix into section of func instead
    
    * edits docker
    
    * dockerfile pip
    
    * scared
    
    * test debug flag off
    
    * Update main.py
    
    * removes reset of tz in wordle.py
    
    * Update README.md
    
    * cleanup
    
    * refactoring ui
    
    * finalizing ui
    
    * fixes emote
    
    * adds solution to $today
    
    * cleans up files in git
    
    * update ui to be more intuitive
    
    * cleanup and a few changes
    
    * bug fix score not showing for past wordle
    
    * adds band-aid to previous wordles slipping into ''
    
    * adds wordle command to help
    
    * fixes score ui bug.. again
    
    * formats the date a bit nicer
    
    * updates emote used
    
    * Update README.md

--- src/main.py
+++ src/main.py
@@ -1,20 +1,210 @@
-from dotenv import load_dotenv, dotenv_values
+import os, time
+from datetime import datetime
+from dotenv import dotenv_values
 import discord
+import pandas
+import humanize
+from wordle import is_wordle_share, find_try_ratio, WordleHistoryState, find_wordle_id, find_solution
 
 config = dotenv_values(".env")
 
-client = discord.Client()
+# Custom VARS for custom situational logic. Does not affect using this bot in other servers.
+WORDLE_DAILY_CHANNEL = 937390252576886845
+MAIN_CHANNEL = 731718737694162977
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+TEST_IN_TEST_SV = True  # Used when you want to pull from ^ instead of where the cmd is coming from.
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+def __make_leaderboard_embed__(title: str, df: pandas.DataFrame):
+    """
+    :param: df: pandas.DataFrame
+        Requires DataFrame with the following dtypes
+            player_id                     object
+            total_games                    int64
+            avg_won_on_attempt           float64
+            win_percent                  float64
+            started_date          datetime64[ns]
+    """
+    embed = discord.Embed(title=f"__**{title}:**__", color=discord.Color.from_rgb(204, 0, 0))
+    for index, row in df.iterrows():
+        embed.add_field(name=f'**{index + 1}) {row.player_id}**',
+                        value=f'Total Games: `{row.total_games}`\n'
+                              f'> Averaging: `{row.avg_won_on_attempt}/6`\n'
+                              f'> Win %: `{row.win_percent}`\n'
+                              f'> Playing since: {row.started_date.strftime("%l:%M%p %Z on %b %d, %Y")}',
+                        inline=False)
+    return embed
 
+
+def __make_wordle_day_embed__(wid: int, avg_turn_won: float, percent_of_winners: float, df: pandas.DataFrame):
+    """
+    :param: df pandas.DataFrame
+    with columns: *sorted
+        player_id                  object
+        wordle_id                  object
+        won_on_try_num            float64
+        total_num_tries            object
+        created_date       datetime64[ns]
+    """
+    embed = discord.Embed(title=f"__**Wordle {wid}:**__", color=discord.Color.from_rgb(255, 255, 0))
+    last_day_for_wid = None  # should probably be part of the input ui for SoC, but too lazy.
+    # better practice would be to utilize ui models, but maybe that's too much for python
+    for index, row in df.iterrows():
+        embed.add_field(name=f'**{index + 1}) {row.player_id}**',
+                        value=f'> `{row.won_on_try_num}/6`\n' +
+                              (f'> *{humanize.naturaltime(row.created_date)}*'
+                               if row.created_date.date() == datetime.now().date()
+                               else f"> *{row.created_date.strftime('%-I:%M%p')}*"),
+                        inline=True)
+        last_day_for_wid = row.created_date
+    embed.add_field(name=f'__**Overall Daily Statistics**__',
+                    value=f'<:thonking:726838160809787464>',
+                    inline=False)
+    embed.add_field(name=f"How many won?",
+                    value=f"> `{percent_of_winners * 100}`%")
+    embed.add_field(name=f"Average Attempts",
+                    value=f"> `{avg_turn_won}`")
+    embed.add_field(name=f"Solution:",
+                    value=f"> ||{find_solution(wid=wid).upper()}||")
+    embed.set_footer(text=f"{last_day_for_wid.strftime('%B %d, %Y')}")
+    return embed
+
+
+class WordleClient(discord.Client):
+
+    def __init__(self, *, loop=None, **options):
+        super().__init__(loop=loop, **options)
+        self.channel_states = dict()  # <channel_id str: WordleHistoryState>
+
+    async def __channel_import__(self, channel_id: int):
+        """
+        Imports last 1k messages into the bot state.
+        There is additional custom logic for my personal server if the conditions are met.
+        """
+        channel = self.get_channel(channel_id)
+        messages = await channel.history(limit=1000).flatten()
+        self.channel_states[channel_id] = WordleHistoryState()
+        for message in messages:
+            if message.author.bot is True:
+                continue
+            await self.__add_to_state__(message)
+
+        # Custom logic for "ToG" Server
+        # We have the first few wordle sessions in the main channel,
+        # so we want to port it over to the wordle channel board instead.
+        if channel_id == WORDLE_DAILY_CHANNEL:
+            channel = self.get_channel(MAIN_CHANNEL)
+            messages = await channel.history(limit=1000).flatten()
+            for message in messages:
+                await self.__add_to_state__(message, WORDLE_DAILY_CHANNEL)
+
+    async def __add_to_state__(self, message: discord.Message, overrided_leaderboard_id: int = None):
+        """
+        Process message from anywhere and add it to the state of the bot.
+
+        :param discord.Message message: 
+            - Message being sent.
+            - Adds the Wordle Game to the the channel's state.
+        :param str overrided_leaderboard_id: 
+            Instead of adding this message to the original channel's leader-board, you may override it to another board.
+            This functionality is built-in for consolidating leader-boards from multiple channels into just one.
+            (if needed)
+        """
+        channel_id = overrided_leaderboard_id if overrided_leaderboard_id else message.channel.id
+        message_content = message.content.strip()
+        if message.author.bot is True or not is_wordle_share(message_content):
+            return
+        # if we're processing a message and we haven't imported that data yet, let's import it here.
+        # typically, we only add to state (memory) on demand to lower cpu/mem usage.
+        # but we should make an adjustment on this case.
+        if channel_id not in self.channel_states:
+            await self.__channel_import__(channel_id)
+        header = message_content.split('\n')[0]
+        wordle_id = find_wordle_id(header)
+        won_on_try, max_tries = find_try_ratio(header)
+        self.channel_states[channel_id].add_wordle(player_id=message.author.display_name,
+                                                   wordle_id=wordle_id,
+                                                   won_on_try_num=won_on_try,
+                                                   total_num_tries=max_tries,
+                                                   created_date=message.created_at)
+
+    async def on_ready(self):
+        print('We have logged in as {0.user}'.format(self))
+
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+
+        if message.content == '$shutdown':
+            exit(0)
+
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello!\n v0.0.3 \nBetter Wordle Bot says hello!')
+            return
+
+        if message.content == '$restart-state':
+            await self.__channel_import__(message.channel.id)
+            await message.channel.send('The wordle bot has restarted.')
+
+        if message.content == '$leaderboard':
+            channel_id = message.channel.id if not TEST_IN_TEST_SV else WORDLE_DAILY_CHANNEL
+            if channel_id not in self.channel_states:
+                await self.__channel_import__(channel_id)
+            all_stats_df = self.channel_states \
+                .get(channel_id) \
+                .compute_all_stats_df()
+            embed = __make_leaderboard_embed__(
+                "All-time Leaderboard", all_stats_df)
+            await message.channel.send(embed=embed)
+            return
+
+        if message.content == '$today':
+            channel_id = message.channel.id if not TEST_IN_TEST_SV else WORDLE_DAILY_CHANNEL
+            if channel_id not in self.channel_states:
+                await self.__channel_import__(channel_id)
+            wid, avg_turn_won, percent_of_winners, df = self.channel_states.get(
+                channel_id
+            ).compute_daily_df()
+            embed = __make_wordle_day_embed__(wid, avg_turn_won, percent_of_winners, df)
+            await message.channel.send(embed=embed)
+            return
+
+        if message.content.startswith('$wordle'):
+            channel_id = message.channel.id if not TEST_IN_TEST_SV else WORDLE_DAILY_CHANNEL
+            wordle_id = int(message.content.split(" ")[1])
+            if channel_id not in self.channel_states:
+                await self.__channel_import__(channel_id)
+            wid, avg_turn_won, percent_of_winners, df = self.channel_states.get(
+                channel_id
+            ).compute_day_df_for_wordle(wordle_id)
+            embed = __make_wordle_day_embed__(wid, avg_turn_won, percent_of_winners, df)
+            await message.channel.send(embed=embed)
+            return
+
+        if message.content.startswith('$help'):
+            await message.channel.send(
+                'If this is an emergency, please dial 911. \n'
+                'Supported commands: `$today`,`$leaderboard`, `$wordle <id>`, `$hello`, `$help`'
+            )
+            return
+
+        # this command is to setup the time on the machine,
+        # i have not put it as a start up step
+        # because i have not found root cause
+        if message.content.startswith('$time'):
+            if os.environ.get('TZ') == 'US/Eastern':
+                await message.channel.send(f"TZ is EST > {time.strftime('%l:%M%p %Z on %b %d, %Y')}")
+                return
+            before = time.strftime('%l:%M%p %Z on %b %d, %Y')
+            os.environ['TZ'] = 'US/Eastern'  # set new timezone
+            time.tzset()
+            after = time.strftime('%l:%M%p %Z on %b %d, %Y')
+            await message.channel.send(f"TZ: {before} -> {after}")  # before timezone change
+            return
+
+        # Process these messages so we don't need to recalculate everything again.
+        await self.__add_to_state__(message)
+
+
+client = WordleClient()
 client.run(config["BOT_TOKEN"])
---
+++ src/wordle.py
@@ -0,0 +1,142 @@
+import re
+import numpy as np
+import pandas as pd
+from datetime import datetime, date
+
+# the words are hardcoded into the game and WID is really just index
+all_wordle_solutions = np.load("words.npy")
+
+
+# Helpers
+
+def find_solution(wid: int) -> str:
+    return all_wordle_solutions[wid]
+
+
+def find_wordle_id(wordle_share_msg_header: str):
+    return int(str(wordle_share_msg_header[7:10]))
+
+
+def is_wordle_share(msg: str):
+    return re.match(r'Wordle \d\d\d ./\d\n\n[⬛⬜🟩🟨]{5}', msg) is not None
+
+
+def find_try_ratio(wordle_share_msg_header: str):
+    """
+    returns: tuple (attempt, of_tries)
+    """
+    header = wordle_share_msg_header
+    won_on_try = int(header[-3]) if header[-3].isdigit() else None
+    max_attempts = int(header[-1])
+    return won_on_try, max_attempts
+
+
+# State
+
+
+class WordleHistoryState:
+
+    def __init__(self):
+        self.wordle_df = pd.DataFrame(columns=[
+            'player_id',  # str
+            'wordle_id',  # int
+            'won_on_try_num',  # optional<int>
+            'total_num_tries',  # int
+            'created_date'  # int
+        ])
+
+    # TODO: Remove this and just prevent duplicates in add
+    def __prepare_for_computation__(self):
+        """
+        We can lazily prepare for a computation if needed here.
+            - Prevents duplicate counts of shares.
+                - it would be better if we can just overwrite duplicates on add_wordle()
+        """
+        self.wordle_df = self.wordle_df.drop_duplicates(subset=['player_id', 'wordle_id'], keep='last')
+
+    def add_wordle(self, player_id: str, wordle_id: int, won_on_try_num: int, total_num_tries: int,
+                   created_date: datetime):
+        self.wordle_df = self.wordle_df.append({'player_id': player_id,
+                                                'wordle_id': wordle_id,
+                                                'won_on_try_num': won_on_try_num,
+                                                'total_num_tries': total_num_tries,
+                                                'created_date': created_date},
+                                               ignore_index=True)
+
+    def compute_all_stats_df(self):  #
+        """
+        :returns: pandas.DataFrame with columns: *sorted
+            index                          int64
+            player_id                        str
+            total_games                    int64
+            avg_won_on_attempt           float64
+            win_percent                  float64
+            started_date          datetime64[ns]
+        """
+        self.__prepare_for_computation__()
+        wordle_df = self.wordle_df.copy()
+        all_stats_df = pd.DataFrame()
+        groupedby_players = wordle_df.groupby(wordle_df.player_id)
+        all_stats_df["total_games"] = groupedby_players.size()
+        all_stats_df["avg_won_on_attempt"] = groupedby_players.won_on_try_num.mean()
+        # there must be a simpler way for getting each person's WIN percentage
+        all_stats_df['win_percent'] = (wordle_df[wordle_df['won_on_try_num'].notna()].groupby(
+            wordle_df.player_id).size() / wordle_df.groupby(wordle_df.player_id).size()) * 100
+        all_stats_df['started_date'] = groupedby_players.created_date.min()
+        return all_stats_df.sort_values(["avg_won_on_attempt", "win_percent", "started_date"],
+                                        ascending=(True, False, True)).round(1).reset_index()
+
+    def compute_day_df_for_wordle(self, wordle_id: int, top: int = None):
+        """
+        :returns:
+            avg attempts for wins
+            avg of people who won
+            pandas.DataFrame with columns: *sorted
+                player_id                  object
+                wordle_id                  int64
+                won_on_try_num            float64
+                total_num_tries            object
+                created_date       datetime64[ns]
+        """
+        self.__prepare_for_computation__()
+        df = self.wordle_df.copy()
+        df.wordle_id = pd.to_numeric(df.wordle_id)
+        df = df.loc[wordle_id == df.wordle_id]
+        percent_of_winners = df.won_on_try_num.notna().mean()
+        avg_turn_won = df.won_on_try_num.mean()
+        if top:
+            df = df.nlargest(top, 'won_on_try_num')
+
+        df = df.sort_values(["won_on_try_num", "created_date"], ascending=(True, True))
+        df = df.reset_index(drop=True)
+        return wordle_id, avg_turn_won, percent_of_winners, df
+
+    def compute_daily_df(self, top: int = None):
+        """
+        :returns:
+            avg attempts for wins
+            avg of people who won
+            pandas.DataFrame with columns: *sorted
+                player_id                  object
+                wordle_id                  int64
+                won_on_try_num            float64
+                total_num_tries            object
+                created_date       datetime64[ns]
+        """
+        self.__prepare_for_computation__()
+        df = self.wordle_df.copy()
+        df = df.loc[(date.today() == df['created_date'].dt.date)]
+        df.wordle_id = pd.to_numeric(df.wordle_id)
+        wid = df.wordle_id.value_counts().idxmax()
+        percent_of_winners = df.won_on_try_num.notna().mean()
+        df.won_on_try_num = pd.to_numeric(df.won_on_try_num)
+        avg_turn_won = df.won_on_try_num.mean()
+        # [band-aid] somehow, other wordles are slipping in, so let's filter those out here by only showing the most
+        df = df[df.wordle_id == df.wordle_id.mode()[0]]
+        #
+        if top:
+            df = df.nlargest(top, 'won_on_try_num')
+
+        df = df.sort_values(["won_on_try_num", "created_date"], ascending=(True, True))
+        df = df.reset_index(drop=True)
+        return wid, avg_turn_won, percent_of_winners, df
